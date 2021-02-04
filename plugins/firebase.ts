f6cc299e0412f8268529066866c4ab7e8dd91d94
hizukayudai@gmail.com <09120912Abcd>
2021-02-04 23:37:36 +0900

    Update firebase.ts

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -2,13 +2,13 @@ import  Firebase from "firebase/app"
 import "firebase/auth"
 
 const config = {
-  appId: 'AIzaSyCQ35LnhgTIB9qJAZvIjVYyvPtU_WImr5A',
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
-  messagingSenderId: process.env.FIREBASE_SEND_ID,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyCQ35LnhgTIB9qJAZvIjVYyvPtU_WImr5A",
+  authDomain: "cloud-beat-pro.firebaseapp.com",
+  projectId: "cloud-beat-pro",
+  storageBucket: "cloud-beat-pro.appspot.com",
+  messagingSenderId: "1041574609748",
+  appId: "1:1041574609748:web:bf9339233c5bde47bb3ec1",
+  measurementId: "G-8QWCNWBQL2"
 }
 
 const firebase: Firebase.app.App = Firebase.initializeApp(config)
