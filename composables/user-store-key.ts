a29095fa1d2303808f59dad69f958016ea6d3fef
hizukayudai@gmail.com <09120912Abcd>
2021-05-03 18:37:54 +0900

    remove project

--- composables/user-store-key.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionKey } from '@nuxtjs/composition-api'
-import { UserStore } from '~/composables/user-store'
-
-const UserStoreKey: InjectionKey<UserStore> = Symbol('UserStore')
-export default UserStoreKey
